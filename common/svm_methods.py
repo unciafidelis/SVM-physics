4e6098131141faf62dc2471e17beb6e77bc72558
Andres Ramirez <anramire@mail.desy.de>
2022-10-14 10:55:23 +0900

    add rbf kernel

--- common/svm_methods.py
+++ common/svm_methods.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 from sklearn.svm import SVC, LinearSVC
 
@@ -25,9 +26,39 @@ class LinearPrecomputed():
         return np.array([[self.linear_explicit(i, j) for j in self.y ] for i in self.x])
 
     @staticmethod
-    def linear_np(i, j):
-        return np.dot(i, j.T)
+    def linear_np(v1, v2):
+        return np.dot(v1, v2.T)
    
     def numpy_calc(self):
         return np.array([[self.linear_np(i, j) for j in self.y ] for i in self.x])
 
+
+class RBFPrecomputed():
+    """
+    Class that computes by hand the RBF kernel
+    Returns a numpy array that is the gaussian transform of the input matrices
+    """
+    def __init__(self, x, y=None, gamma=0.01):
+        self.x = np.array(x)
+        self.gamma = gamma
+        if y is None:
+            self.y = np.array(x)
+        else:
+            self.y = np.array(y)
+
+    def rbf_explicit(self, v1, v2):
+        norm = 0
+        for i in range(0, len(v1)):
+            norm += (v1[i]-v2[i]) * (v1[i]-v2[i])
+        return math.exp(-(norm) * self.gamma)
+  
+    def explicit_calc(self):
+        return np.array([[self.rbf_explicit(i, j) for j in self.y ] for i in self.x])
+
+    def rbf_np(self, v1, v2):
+        norm = ((v1-v2).dot(v1-v2))
+        return math.exp(-(norm) * self.gamma)
+        # return np.dot(i, j.T)
+   
+    def numpy_calc(self):
+        return np.array([[self.rbf_np(i, j) for j in self.y ] for i in self.x])
--- tests/test_svm_methods.py
+++ tests/test_svm_methods.py
@@ -5,14 +5,12 @@ import pytest
 import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score,precision_score
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVC, LinearSVC
 # framework includes
 from data.data_preparation import DataPreparation
 
 data = DataPreparation(path="../data/", GA_selection=False)
-X_train, y_train, X_test, y_test = data.dataset(sample_name="titanic", sampling=False, split_sample=0.6)
+X_train, y_train, X_test, y_test = data.dataset(sample_name="heart", sampling=False, split_sample=0.5)
 
 
 class TestLinearPrecomputed(unittest.TestCase):
@@ -38,12 +36,43 @@ class TestLinearPrecomputed(unittest.TestCase):
         matrix_np = test_linear.numpy_calc()
         model_np = SVC(kernel="precomputed")
         model_np.fit(matrix_np, y_train)
-        matrix_test_np = matrix_test.explicit_calc()
+        matrix_test_np = matrix_test.numpy_calc()
         y_pred_np = model_np.predict(matrix_test_np)
         acc_np = accuracy_score(y_test, y_pred_np)
         prc_np = precision_score(y_test, y_pred_np)
         # default(original) kernel
         model_og = SVC(kernel="linear")
+        model_og.fit(X_train, y_train)
+        y_pred_og = model_og.predict(X_test)
+        acc_og = accuracy_score(y_test, y_pred_og)
+        prc_og = precision_score(y_test, y_pred_og)
+        self.assertEqual(acc_og, acc_ep)
+        self.assertEqual(prc_og, prc_ep)
+        self.assertEqual(acc_og, acc_np)
+        self.assertEqual(prc_og, prc_np)
+        
+    def test_rbf_precomputed(self):
+        from common.svm_methods import RBFPrecomputed
+        test_rbf = RBFPrecomputed(X_train)
+        matrix_test = RBFPrecomputed(X_test, X_train)
+        # precomputed kernel, explicit calculation
+        matrix_ep = test_rbf.explicit_calc()
+        model_ep = SVC(kernel="precomputed")
+        model_ep.fit(matrix_ep, y_train)
+        matrix_test_ep = matrix_test.explicit_calc()
+        y_pred_ep = model_ep.predict(matrix_test_ep)
+        acc_ep = accuracy_score(y_test, y_pred_ep)
+        prc_ep = precision_score(y_test, y_pred_ep)
+        # precomputed kernel, numpy calculation
+        matrix_np = test_rbf.numpy_calc()
+        model_np = SVC(kernel="precomputed")
+        model_np.fit(matrix_np, y_train)
+        matrix_test_np = matrix_test.numpy_calc()
+        y_pred_np = model_np.predict(matrix_test_np)
+        acc_np = accuracy_score(y_test, y_pred_np)
+        prc_np = precision_score(y_test, y_pred_np)
+        # default(original) kernel
+        model_og = SVC(kernel="rbf", gamma=0.01)
         model_og.fit(X_train, y_train)
         y_pred_og = model_og.predict(X_test)
         acc_og = accuracy_score(y_test, y_pred_og)
