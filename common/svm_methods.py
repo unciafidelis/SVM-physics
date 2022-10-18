288b41ee510af01c46e9e6883c0d44528a9db02a
Alejandro Morgan <unciafidelis@gmail.com>
2022-10-18 14:53:05 -0500

    PEP8 style mayor corrections implementation

--- common/svm_methods.py
+++ common/svm_methods.py
@@ -8,6 +8,7 @@ class LinearPrecomputed():
     Class that computes by hand a linear kernel
     Returns a numpy array that is the dot product of the input matrices
     """
+
     def __init__(self, x, y=None):
         self.x = np.array(x)
         if y is None:
@@ -21,16 +22,18 @@ class LinearPrecomputed():
         for i in range(0, len(v1)):
             prod += v1[i] * v2[i]
         return prod
-  
+
     def explicit_calc(self):
-        return np.array([[self.linear_explicit(i, j) for j in self.y ] for i in self.x])
+        return np.array([[self.linear_explicit(i, j)
+                        for j in self.y] for i in self.x])
 
     @staticmethod
     def linear_np(v1, v2):
         return np.dot(v1, v2.T)
-   
+
     def numpy_calc(self):
-        return np.array([[self.linear_np(i, j) for j in self.y ] for i in self.x])
+        return np.array([[self.linear_np(i, j) for j in self.y]
+                        for i in self.x])
 
 
 class RBFPrecomputed():
@@ -38,6 +41,7 @@ class RBFPrecomputed():
     Class that computes by hand a RBF kernel
     Returns a numpy array that is the gaussian transform of the input matrices
     """
+
     def __init__(self, x, y=None, gamma=0.01):
         self.x = np.array(x)
         self.gamma = gamma
@@ -49,18 +53,19 @@ class RBFPrecomputed():
     def rbf_explicit(self, v1, v2):
         norm = 0
         for i in range(0, len(v1)):
-            norm += (v1[i]-v2[i]) * (v1[i]-v2[i])
+            norm += (v1[i] - v2[i]) * (v1[i] - v2[i])
         return math.exp(-(norm) * self.gamma)
-  
+
     def explicit_calc(self):
-        return np.array([[self.rbf_explicit(i, j) for j in self.y ] for i in self.x])
+        return np.array([[self.rbf_explicit(i, j)
+                        for j in self.y] for i in self.x])
 
     def rbf_np(self, v1, v2):
-        norm = ((v1-v2).dot(v1-v2))
+        norm = ((v1 - v2).dot(v1 - v2))
         return math.exp(-(norm) * self.gamma)
-   
+
     def numpy_calc(self):
-        return np.array([[self.rbf_np(i, j) for j in self.y ] for i in self.x])
+        return np.array([[self.rbf_np(i, j) for j in self.y] for i in self.x])
 
 
 class PolyPrecomputed():
@@ -68,6 +73,7 @@ class PolyPrecomputed():
     Class that computes by hand a polynomial kernel
     Returns a numpy array that is the gaussian transform of the input matrices
     """
+
     def __init__(self, x, y=None, gamma=1, deg=1, coef=0):
         self.x = np.array(x)
         self.gamma = gamma
@@ -84,17 +90,18 @@ class PolyPrecomputed():
             prod += v1[i] * v2[i]
         prod = self.gamma * prod + self.coef
         return math.pow(prod, self.deg)
-  
+
     def explicit_calc(self):
-        return np.array([[self.poly_explicit(i, j) for j in self.y ] for i in self.x])
+        return np.array([[self.poly_explicit(i, j)
+                        for j in self.y] for i in self.x])
 
     def poly_np(self, v1, v2):
         prod = np.dot(v1, v2.T)
         prod = self.gamma * prod + self.coef
         return math.pow(prod, self.deg)
-   
+
     def numpy_calc(self):
-        return np.array([[self.poly_np(i, j) for j in self.y ] for i in self.x])
+        return np.array([[self.poly_np(i, j) for j in self.y] for i in self.x])
 
 
 class KernelSum():
@@ -103,6 +110,7 @@ class KernelSum():
     result = a * A + b * B + ...
     Input values are gram matrices (kernel matrices)
     """
+
     def __init__(self, kernels=[]):
         if len(kernels) == 0:
             print("No input kernels. Goodbye!")
@@ -115,7 +123,3 @@ class KernelSum():
         for kernel in self.kernels:
             sum += kernel[0] * kernel[1]
         return sum
-
-
-
-
